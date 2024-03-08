2b7d4bafccc1cd53eb30b54b663f6b52759137b7
Klaus Dimde <klaus@dimde.info>
2024-03-08 14:00:58 +0000

    Moved example array property in own file

---
+++ tests/TestSupport/NonAbstractArrayProperty.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Sunhill\Properties\Tests\TestSupport;
+
+use Sunhill\Properties\Properties\AbstractArrayProperty;
+
+class NonAbstractArrayProperty extends AbstractArrayProperty
+{
+    
+    public function count(): int
+    {
+        return 2;
+    }
+    
+    public function offsetExists(mixed $offset): bool
+    {
+        
+    }
+    
+    public function offsetGet(mixed $offset): mixed
+    {
+        
+    }
+    
+    public function offsetSet(mixed $offset, mixed $value): void
+    {
+        
+    }
+    
+    public function offsetUnset(mixed $offset): void
+    {
+        
+    }
+    
+    public function current(): mixed
+    {
+        
+    }
+    
+    
+    public function key(): mixed
+    {
+        
+    }
+    
+    public function next(): void
+    {
+        
+    }
+    
+    public function rewind(): void
+    {
+        
+    }
+    
+    public function valid(): bool
+    {
+        
+    }
+    
+    public function isValid($test): bool
+    {
+        
+    }
+    
+}
--- tests/Unit/Properties/AbstractArrayPropertyTest.php
+++ tests/Unit/Properties/AbstractArrayPropertyTest.php
@@ -1,68 +1,8 @@
 <?php
 
 use Sunhill\Properties\Tests\TestCase;
-use Sunhill\Properties\Properties\AbstractArrayProperty;
+use Sunhill\Properties\Tests\TestSupport\NonAbstractArrayProperty;
 
-class NonAbstractArrayProperty extends AbstractArrayProperty
-{
-     
-    public function count(): int
-    {
-        return 2;
-    }
-    
-    public function offsetExists(mixed $offset): bool
-    {
-        
-    }
-    
-    public function offsetGet(mixed $offset): mixed
-    {
-        
-    }
-    
-    public function offsetSet(mixed $offset, mixed $value): void
-    {
-        
-    }
-    
-    public function offsetUnset(mixed $offset): void
-    {
-        
-    }
-    
-    public function current(): mixed
-    {
-        
-    }
-    
-    
-    public function key(): mixed
-    {
-        
-    }
-    
-    public function next(): void
-    {
-        
-    }
-    
-    public function rewind(): void
-    {
-        
-    }
-    
-    public function valid(): bool
-    {
-        
-    }
-    
-    public function isValid($test): bool
-    {
-        
-    }
-    
-}
 
 class AbstractArrayPropertyTest extends TestCase
 {
