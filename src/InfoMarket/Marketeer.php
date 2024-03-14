176038f3f3bf06fd6e56b56ceb96abb22af6f43b
Klaus Dimde <klaus@dimde.info>
2024-03-14 08:25:52 +0100

    Implemented stub for marketeer and market

---
+++ src/InfoMarket/Marketeer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @file Marketeer.php
+ * Provides the Marketeer class as a base class for marketeers.
+ *
+ * @author Klaus Dimde
+ * ----------------------------------------------------------------------
+ * Lang en
+ * Reviewstatus: 2024-03-14
+ * Localization: unknown
+ * Documentation: all public
+ * Tests: Unit/InfoMarket/Marketeer.php
+ * Coverage: unknown
+ * PSR-State: complete
+ */
+
+namespace Sunhill\Properties\InfoMarket;
+
+use Sunhill\Properties\Properties\AbstractRecordProperty;
+
+class Marketeer extends AbstractRecordProperty
+{
+    
+}
