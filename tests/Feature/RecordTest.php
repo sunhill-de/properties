d1022d5e648cd10f0903dee4d64cf6341cb1898b
Klaus Dimde <klaus@dimde.info>
2024-04-02 22:31:15 +0200

    More feature tests

--- tests/Feature/RecordTest.php
+++ tests/Feature/RecordTest.php
@@ -12,6 +12,14 @@ test('initialize a record with a callback storage and read it', function()
     expect($test->sample_string)->toBe('ABC');
 });
 
+test('initialize a record with a callback storage and read an array element', function()
+{
+    $test = new SampleCallbackProperty();
+    $test->setName('TestCallback');
+    
+    expect($test->sample_array[1])->toBe('ABC');
+});
+
 test('initialize a record with a callback storage and write to it', function()
 {
     $test = new SampleCallbackProperty();
