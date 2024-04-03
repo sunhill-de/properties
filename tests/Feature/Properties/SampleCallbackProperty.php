8c4d45405cd609c67f492239b24d400a4b9a0aaa
Klaus Dimde <klaus@dimde.info>
2024-04-03 15:01:47 +0200

    Some feature tests of marketeers

--- tests/Feature/Properties/SampleCallbackProperty.php
+++ tests/Feature/Properties/SampleCallbackProperty.php
@@ -2,17 +2,18 @@
 
 namespace Sunhill\Properties\Tests\Feature\Properties;
 
-use Sunhill\Properties\Properties\RecordProperty;
 use Sunhill\Properties\Types\TypeVarchar;
 use Sunhill\Properties\Types\TypeInteger;
 use Sunhill\Properties\Tests\Feature\Storages\SampleCallbackStorage;
 use Sunhill\Properties\Properties\ArrayProperty;
+use Sunhill\Properties\InfoMarket\Marketeer;
 
-class SampleCallbackProperty extends RecordProperty
+class SampleCallbackProperty extends Marketeer
 {
     
     protected function initializeElements()
     {
+        $this->setName('callback');
         $this->addElement('sample_string', new TypeVarchar());
         $this->addElement('sample_integer', new TypeInteger());
         $this->addElement('sample_array', new ArrayProperty())->setAllowedElementTypes(TypeVarchar::class);
