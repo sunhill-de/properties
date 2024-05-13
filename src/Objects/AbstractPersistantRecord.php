5a9eb592af2587c8a3306739e09279a1e99bff56
Klaus Dimde <klaus@dimde.info>
2024-05-13 15:42:12 +0200

    Implemented the common methods for AbstractPersistantRecord

--- src/Objects/AbstractPersistantRecord.php
+++ src/Objects/AbstractPersistantRecord.php
@@ -3,13 +3,18 @@
 namespace Sunhill\Properties\Objects;
 
 use Sunhill\Properties\Properties\AbstractRecordProperty;
+use Sunhill\Properties\Properties\AbstractProperty;
 
 class AbstractPersistantRecord extends AbstractRecordProperty
 {
     
+    protected $elements = [];
+    
+    protected $element_references = [];
+    
     public function __construct()
     {
-        $descriptor = new ObjectDescriptor();
+        $descriptor = new ObjectDescriptor($this);
         $this->initializeProperties($descriptor);
     }
     
@@ -20,7 +25,7 @@ class AbstractPersistantRecord extends AbstractRecordProperty
     
     public function getElementNames()
     {
-        
+        return array_keys($this->elements);    
     }
     
     public function getElements()
@@ -43,4 +48,44 @@ class AbstractPersistantRecord extends AbstractRecordProperty
         
     }
     
+    public function embedElement(string $classname): AbstractProperty
+    {
+        
+    }
+    
+    public function includeElement(string $classname): AbstractProperty
+    {
+        
+    }
+    
+    public function appendElement(string $element_name, string $class_name): AbstractProperty
+    {
+        
+    }
+    
+    public function hasInclude(string $classname): bool
+    {
+        
+    }
+    
+    public function hasEmbed(string $classname): bool
+    {
+        
+    }
+    
+    public function isDirty(): bool
+    {
+        
+    }
+    
+    public function commit()
+    {
+        
+    }
+    
+    public function rollback()
+    {
+        
+    }
 }
+
--- tests/Unit/Objects/PersistantRecord/PersistantRecordTest.php
+++ tests/Unit/Objects/PersistantRecord/PersistantRecordTest.php
@@ -1,43 +1,232 @@
 <?php
 
 use Sunhill\Properties\Tests\TestCase;
-use Sunhill\Properties\Objects\ObjectDescriptor;
 use Sunhill\Properties\Types\TypeInteger;
-use Sunhill\Properties\Tests\Unit\Objects\PersistantRecord\Samples\SampleAbstractPersistantRecord;
 use Sunhill\Properties\Objects\Exceptions\TypeCannotBeEmbeddedException;
+use Sunhill\Properties\Tests\Unit\Objects\PersistantRecord\Samples\EmptyPersistantRecord;
+use Sunhill\Properties\Facades\Properties;
+use Sunhill\Properties\Properties\AbstractRecordProperty;
+use Sunhill\Properties\Types\TypeVarchar;
+use Sunhill\Properties\Objects\Exceptions\TypeAlreadyEmbeddedException;
+use Sunhill\Properties\Properties\Exceptions\DuplicateElementNameException;
 
 uses(TestCase::class);
 
-test('Persistant record calls initializeProperties', function() 
-{
-    $integer = \Mockery::mock(TypeInteger::class);
-    $descriptor = \Mockery::mock(ObjectDescriptor::class);
-    $descriptor->shouldReceive('integer')->with('testint')->andReturn($integer);
-    $test = new SampleAbstractPersistantRecord();    
-});
-
 test('appendElement() works', function()
 {
+    $test = new EmptyPersistantRecord();
+    $integer = \Mockery::mock(TypeInteger::class);
+    $integer->shouldReceive('setOwner')->with($test)->andReturn($integer);
+    Properties::shouldReceive('getNamespaceOfProperty')->with('integer')->andReturn($integer);
+    $test->appendElement('testint','integer');
     
+    expect($test->hasElement('testint'))->toBe(true);
 });
 
 test('embedElement() works', function() 
 {
+    $test = new EmptyPersistantRecord();
     
+    $integer = \Mockery::mock(TypeInteger::class);
+    $integer->shouldReceive('setOwner')->with($test)->andReturn($integer);
+    $varchar = \Mockery::mock(TypeVarchar::class);
+    $varchar->shouldReceive('setOwner')->with($test)->andReturn($varchar);
+
+    $record = \Mockery::mock(AbstractRecordProperty::class);
+    $record->shouldReceive('getElements')->andReturn(
+       [
+           'intval'=>$integer,
+           'charval'=>$varchar
+           ]
+       );
+    
+    Properties::shouldReceive('getNamespaceOfProperty')->with('integer')->andReturn($integer);
+    Properties::shouldReceive('getNamespaceOfProperty')->with('varchar')->andReturn($varchar);
+    Properties::shouldReceive('getNamespaceOfProperty')->with('TestRecord')->andReturn($record);
+    
+    $test->embedElement('TestRecord');
+    
+    expect($test->hasElement('intval'))->toBe(true);
+    expect($test->hasElement('charval'))->toBe(true);
+    expect($test->hasEmbed('TestRecord'))->toBe(true);
+    expect($test->hasInclude('TestRecord'))->toBe(false);
 });
+
+test('embedElement() fails with duplicate', function()
+{
+    $test = new EmptyPersistantRecord();
+    
+    $integer = \Mockery::mock(TypeInteger::class);
+    $integer->shouldReceive('setOwner')->with($test)->andReturn($integer);
+    $varchar = \Mockery::mock(TypeVarchar::class);
+    $varchar->shouldReceive('setOwner')->with($test)->andReturn($varchar);
+    
+    $record = \Mockery::mock(AbstractRecordProperty::class);
+    $record->shouldReceive('getElements')->andReturn(
+        [
+            'intval'=>$integer,
+            'charval'=>$varchar
+        ]
+        );
+    
+    Properties::shouldReceive('getNamespaceOfProperty')->with('integer')->andReturn($integer);
+    Properties::shouldReceive('getNamespaceOfProperty')->with('varchar')->andReturn($varchar);
+    Properties::shouldReceive('getNamespaceOfProperty')->with('TestRecord')->andReturn($record);
+    
+    $test->embedElement('TestRecord');
+    $test->embedElement('TestRecord');    
+})->throws(TypeAlreadyEmbeddedException::class);
 
 test('embedElement() fails when called with wrong datatype', function()
 {
+    $test = new EmptyPersistantRecord();
     
+    $integer = \Mockery::mock(TypeInteger::class);
+    Properties::shouldReceive('getNamespaceOfProperty')->with('integer')->andReturn($integer);
+    
+    $test->embedElement('integer');    
 })->throws(TypeCannotBeEmbeddedException::class);
 
 test('includeElement() works', function()
 {
+    $test = new EmptyPersistantRecord();
     
+    $integer = \Mockery::mock(TypeInteger::class);
+    $integer->shouldReceive('setOwner')->with($test)->andReturn($integer);
+    $varchar = \Mockery::mock(TypeVarchar::class);
+    $varchar->shouldReceive('setOwner')->with($test)->andReturn($varchar);
+    
+    $record = \Mockery::mock(AbstractRecordProperty::class);
+    $record->shouldReceive('getElements')->andReturn(
+        [
+            'intval'=>$integer,
+            'charval'=>$varchar
+        ]
+        );
+    
+    Properties::shouldReceive('getNamespaceOfProperty')->with('integer')->andReturn($integer);
+    Properties::shouldReceive('getNamespaceOfProperty')->with('varchar')->andReturn($varchar);
+    Properties::shouldReceive('getNamespaceOfProperty')->with('TestRecord')->andReturn($record);
+    
+    $test->includeElement('TestRecord');
+    
+    expect($test->hasElement('intval'))->toBe(true);
+    expect($test->hasElement('charval'))->toBe(true);
+    expect($test->hasEmbed('TestRecord'))->toBe(false);
+    expect($test->hasInclude('TestRecord'))->toBe(true);    
 });
+
+test('includeElement() fails when already included', function()
+{
+    $test = new EmptyPersistantRecord();
+    
+    $integer = \Mockery::mock(TypeInteger::class);
+    $integer->shouldReceive('setOwner')->with($test)->andReturn($integer);
+    $varchar = \Mockery::mock(TypeVarchar::class);
+    $varchar->shouldReceive('setOwner')->with($test)->andReturn($varchar);
+    
+    $record = \Mockery::mock(AbstractRecordProperty::class);
+    $record->shouldReceive('getElements')->andReturn(
+        [
+            'intval'=>$integer,
+            'charval'=>$varchar
+        ]
+        );
+    
+    Properties::shouldReceive('getNamespaceOfProperty')->with('integer')->andReturn($integer);
+    Properties::shouldReceive('getNamespaceOfProperty')->with('varchar')->andReturn($varchar);
+    Properties::shouldReceive('getNamespaceOfProperty')->with('TestRecord')->andReturn($record);
+    
+    $test->includeElement('TestRecord');    
+    $test->includeElement('TestRecord');
+})->throws(TypeAlreadyEmbeddedException::class);
 
 test('includeElement() fails when called with wrong datatype', function()
 {
+    $test = new EmptyPersistantRecord();
     
+    $integer = \Mockery::mock(TypeInteger::class);
+    Properties::shouldReceive('getNamespaceOfProperty')->with('integer')->andReturn($integer);
+    
+    $test->embedElement('integer');    
 })->throws(TypeCannotBeEmbeddedException::class);
 
+test('embedElement() fails when duplicate name', function()
+{
+    $test = new EmptyPersistantRecord();
+    
+    $integer = \Mockery::mock(TypeInteger::class);
+    $integer->shouldReceive('setOwner')->with($test)->andReturn($integer);
+    $varchar = \Mockery::mock(TypeVarchar::class);
+    $varchar->shouldReceive('setOwner')->with($test)->andReturn($varchar);
+    $record1 = \Mockery::mock(AbstractRecordProperty::class);
+    $record1->shouldReceive('getElements')->andReturn(
+        [
+            'testint'=>$integer,
+        ]
+        );
+    Properties::shouldReceive('getNamespaceOfProperty')->with('integer')->andReturn($integer);
+    Properties::shouldReceive('getNamespaceOfProperty')->with('varchar')->andReturn($varchar);
+    Properties::shouldReceive('getNamespaceOfProperty')->with('EmbedRecord')->andReturn($record1);
+    
+    $test->appendElement('testint','integer');
+    $test->embedElement('EmbedRecord');    
+})->throws(DuplicateElementNameException::class);
+
+test('includeElement() fails when duplicate name', function()
+{
+    $test = new EmptyPersistantRecord();
+    
+    $integer = \Mockery::mock(TypeInteger::class);
+    $integer->shouldReceive('setOwner')->with($test)->andReturn($integer);
+    $varchar = \Mockery::mock(TypeVarchar::class);
+    $varchar->shouldReceive('setOwner')->with($test)->andReturn($varchar);
+    $record1 = \Mockery::mock(AbstractRecordProperty::class);
+    $record1->shouldReceive('getElements')->andReturn(
+        [
+            'testint'=>$integer,
+        ]
+        );
+    Properties::shouldReceive('getNamespaceOfProperty')->with('integer')->andReturn($integer);
+    Properties::shouldReceive('getNamespaceOfProperty')->with('varchar')->andReturn($varchar);
+    Properties::shouldReceive('getNamespaceOfProperty')->with('EmbedRecord')->andReturn($record1);
+    
+    $test->appendElement('testint','integer');
+    $test->includeElement('EmbedRecord');
+})->throws(DuplicateElementNameException::class);
+
+test('getElement*() works', function() 
+{
+    $test = new EmptyPersistantRecord();
+    
+    $integer = \Mockery::mock(TypeInteger::class);
+    $integer->shouldReceive('setOwner')->with($test)->andReturn($integer);
+    $varchar = \Mockery::mock(TypeVarchar::class);
+    $varchar->shouldReceive('setOwner')->with($test)->andReturn($varchar);
+    $record1 = \Mockery::mock(AbstractRecordProperty::class);
+    $record1->shouldReceive('getElements')->andReturn(
+        [
+            'intval1'=>$integer,
+            'charval1'=>$varchar
+        ]
+        );
+    $record2 = \Mockery::mock(AbstractRecordProperty::class);
+    $record2->shouldReceive('getElements')->andReturn(
+        [
+            'intval2'=>$integer,
+            'charval2'=>$varchar
+        ]
+        );
+    Properties::shouldReceive('getNamespaceOfProperty')->with('integer')->andReturn($integer);
+    Properties::shouldReceive('getNamespaceOfProperty')->with('varchar')->andReturn($varchar);
+    Properties::shouldReceive('getNamespaceOfProperty')->with('EmbedRecord')->andReturn($record1);
+    Properties::shouldReceive('getNamespaceOfProperty')->with('IncludeRecord')->andReturn($record2);
+    
+    $test->appendElement('testint','integer');
+    $test->embedElement('EmbedRecord');
+    $test->embedElement('IncludeRecord');
+
+    $names = $test->getElemenmtNames();
+    sort($names);
+    expect($names)->toBe(['charval1','charval2','intval1','intval2','testint']);
+});
